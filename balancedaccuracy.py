f3a7407e1e6a38af19c15ca94ff8d0b4217f2b70
Jonah Gräfe <jonah.greafe@gmail.com>
2024-04-11 20:26:57 +0200

    added bacc

--- balancedaccuracy.py
+++ balancedaccuracy.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 # NOTE: This will be the calculation of balanced accuracy for your classification task
 # The balanced accuracy is defined as the average accuracy for each class. 
@@ -15,14 +16,23 @@ import torch
 # Balanced Accuracy
 class BalancedAccuracy:
     def __init__(self, nClasses):
+        self.nClasses = nClasses
+        self.reset()
         # TODO: Setup internal variables
         # NOTE: It is good practive to all reset() from here to make sure everything is properly initialized
 
     def reset(self):
+        self.matrix = np.zeros((self.nClasses, self.nClasses))
         # TODO: Reset internal states.
         # Called at the beginning of each epoch
 
     def update(self, predictions, groundtruth):
+        predictions = np.argmax(predictions, axis=1)
+    
+        for true_label, pred_label in zip(groundtruth, predictions):
+            self.matrix[true_label][pred_label] += 1
+
+        # self.matrix += self.matrix
         # TODO: Implement the update of internal states
         # based on current network predictios and the groundtruth value.
         #
@@ -34,5 +44,33 @@ class BalancedAccuracy:
         # ground truth class.
 
     def getBACC(self):
+        with np.errstate(divide="ignore", invalid="ignore"):
+            per_class = np.diag(self.matrix) / self.matrix.sum(axis=1)
+
+        return np.mean(per_class)
         # TODO: Calculcate and return balanced accuracy 
         # based on current internal state
+
+
+if __name__ == "__main__":
+    predictions_epoch1 = np.array([[0.1, 0.8, 0.1], [0.3, 0.4, 0.3], [0.5, 0.3, 0.2], [0.7, 0.2, 0.1], [0.2, 0.5, 0.3], [0.1, 0.3, 0.6]])
+    groundtruth_epoch1 = np.array([1, 0, 2, 0, 0, 0])
+
+    predictions_epoch2 = np.array([[0.7, 0.2, 0.1], [0.2, 0.5, 0.3], [0.1, 0.9, 0.6], [0.7, 0.9, 0.1], [0.2, 0.9, 0.3], [0.1, 0.9, 0.6]])
+    groundtruth_epoch2 = np.array([2, 1, 0, 0, 0, 0])
+
+    nClasses = 3
+    bacc = BalancedAccuracy(nClasses)
+
+    # Update after epoch 1
+    bacc.update(predictions_epoch1, groundtruth_epoch1)
+    print("Balanced Accuracy after epoch 1:", bacc.getBACC())
+
+    # Update after epoch 2
+    bacc.update(predictions_epoch2, groundtruth_epoch2)
+    print("Balanced Accuracy after epoch 2:", bacc.getBACC())
+
+    from sklearn.metrics import balanced_accuracy_score
+    predictions_epoch1 = np.array([np.argmax(p) for p in predictions_epoch1])
+    predictions_epoch2 = np.array([np.argmax(p) for p in predictions_epoch2])
+    print(balanced_accuracy_score(np.hstack((groundtruth_epoch1, groundtruth_epoch2)), np.hstack((predictions_epoch1, predictions_epoch2))))
